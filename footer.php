47ba5a82afa9ac0d75322dee5072acbc1ce8fcc6
Kishan Jasani <kishanjasani007@yahoo.in>
2020-08-24 16:50:01 +0530

    Add Header and footer to theme

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Footer Template.
+ * 
+ * @package Jk
+ */
+?>
+<footer>Footer</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,15 +4,15 @@
  * 
  * @package Jk
  */
+
+get_header();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jk</title>
-</head>
-<body>
+
+<div class="content">
     Wordpress
-</body>
-</html>
+</div>
+
+<?php
+
+get_footer();
+    
