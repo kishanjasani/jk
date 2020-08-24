8cad3f86b1c88310da4b45adbe9bdc52af716bdb
Kishan Jasani <kishanjasani007@yahoo.in>
2020-08-24 19:49:36 +0530

    Add theme support options to theme

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jk</title>
     <?php wp_head(); ?>
 </head>
 <body class="<?php body_class(); ?>">
--- inc/classes/class-jk-theme.php
+++ inc/classes/class-jk-theme.php
@@ -23,6 +23,55 @@ class JK_THEME {
     protected function setup_hooks() {
 
         // Actions and filters.
+        add_action( 'after_setup_theme', [ $this, 'setup_theme' ] );
+    }
+
+    public function setup_theme() {
+        add_theme_support( 'title-tag' );
+
+        add_theme_support( 'custom-logo', [
+            'height'      => 100,
+            'width'       => 400,
+            'flex-height' => true,
+            'flex-width'  => true,
+            'header-text' => [ 'site-title', 'site-description' ],
+        ] );
+        
+        add_theme_support( 'custom-background', [
+            'default-color'  => '000000',
+            'default-image'  => '',
+            'default-repeat' => 'no-repeat',    
+        ] );
+
+        add_theme_support( 'post-thumbnails' );
+
+        add_theme_support( 'customize-selective-refresh-widgets' );
+
+        add_theme_support( 'automatic-feed-links' );
+
+        add_theme_support( 
+            'html5',
+            [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+            ]
+        );
+
+        add_theme_support( 'wp-block-styles' );
+
+        add_theme_support( 'align-wide' );
+
+        global $content_width;
+        if ( empty( $content_width ) ) {
+            $content_width = 1240;
+        }
+
+        add_editor_style();
     }
 
 }
