27ee16bd235f2f5281f41e38fed23c4a2d49439f
Kishan Jasani <kishanjasani007@yahoo.in>
2020-08-24 17:39:23 +0530

    Add bootstrap nav menu

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
  */
 ?>
 <!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>">
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,4 +17,9 @@
 
 <?php wp_body_open(); ?>
 
-    <header>Header</header>
+    <div id="page" class="site">
+        <header id="masthead" class="site-header" role="banner">
+            <?php get_template_part( 'template-parts/header/nav' ); ?>
+        </header>
+        <div id="content" class="site-content">
+        
