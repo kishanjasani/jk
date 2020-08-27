c41006f4ff8ac0bf4ffc3de5107a47bbe6c990d2
Kishan Jasani <kishanjasani007@yahoo.in>
2020-08-27 14:10:23 +0530

    Add custom excerpt wrapper to blog

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -6,3 +6,25 @@
  */
 
 ?>
+
+<div class="entry-content">
+    <?php
+    if ( is_single() ) {
+        the_content(
+            sprintf( 
+                wp_kses(
+                    __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'jk' ),
+                    [
+                        'span' => [
+                            'class' => []
+                        ]
+                    ]
+                ),
+                the_title( '<span class="screen-reader-text">"', '"</span>', false ) 
+            )
+        );
+    } else {
+        jk_the_excerpt();
+    }
+    ?>
+</div>
